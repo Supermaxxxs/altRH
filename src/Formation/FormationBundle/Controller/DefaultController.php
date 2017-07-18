9b12fe4297f88e7f9c6b962a3a61fb4284d8213a
Phoenixs <Phoenixs@Dreambreaker>
2017-07-18 14:39:26 +0200

    correction dans le template

--- src/Formation/FormationBundle/Controller/DefaultController.php
+++ src/Formation/FormationBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/index", name="index")
      */
     public function indexAction()
     {
